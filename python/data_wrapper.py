69c2c6d5e2b58418b97caaeb005a8564bf84eccf
sezata <sezata@gmail.com>
2016-11-28 14:32:57 -0500

    renamed mcmc3.py with dg's changes as original mcmc.py file, fixed some python problems, moved some functions from data wrapper, and it's now kind of working

--- python/data_wrapper.py
+++ python/data_wrapper.py
@@ -4,77 +4,80 @@
 # data wrapper for LC data from arXiv:1401.4064v2
 
 import csv
-import scipy as sc
+#import scipy as sc
 import numpy as np
 
-h = 0.673
 
-H0 = 100 * h #km/s/Mpc
+class data_stuff:
+
+    def __init__(self):
+        print "data wrapper init!"
+
+    def inverseHubble(self,z,lm):
+        # Calculates reduced hubble assuming lambda CDM
+        # lm is omega_matter
+        return np.sqrt(lm*np.power((1+z),3)+(1-lm))
+
+    def abs_mag(self,hostmass):
+        # returns true or false given hostmass
+        # arXiv:1401.4064v2 proposed step function for handling differences
+        # for various host galaxies
+        # useless function but this is just to remind me
+        # to put this in the calculation for mu
+        if (hostmass < 10.):
+            return true
+        else:
+            return false
+
+    def dLumi(self,zhel, zcmb, lm, func):
+        # takes heliocentric redshift + CMB frame redshift values
+        # and then outputs a luminosity distance!
+        # arxiv 1601.01451 for reference, eq 2,3
+        distance = (1+zhel)*c*sc.integrate.quad(inverseHubble, 0, zcmb, args=(lm,))/H0
+        return distance
+
+    def import_data(self,inputfile):
+        datafile = open(inputfile, 'r')
+        numLines = 0
+        
+        # returned lists
+        mb_list = []
+        zcmb_list = []
+        zhel_list = []
+        c = []
+        x1_list = []
 
 
-def inverseHubble(z,lm):
-    # Calculates reduced hubble assuming lambda CDM
-    # lm is omega_matter
-    return np.sqrt(lm*np.power((1+z),3)+(1-lm))
+        for line in datafile:
+            numLines = numLines + 1
+            if numLines == 1:
+                continue
+            dataline = line.split()
+            #name zcmb zhel dz mb dmb x1 dx1 color dcolor 3rdvar d3rdvar tmax dtmax cov_m_s cov_m_c cov_s_c set ra dec biascor
+            name = dataline[0]
+            zcmb = float(dataline[1])
+            zhel = float(dataline[2])
+            dz = float(dataline[3])
+            mb = float(dataline[4])
+            dmb = float(dataline[5])
+            x1 = float(dataline[6])
+            dx1 = float(dataline[7])
+            color = float(dataline[8])
+            dcolor = float(dataline[9])
+            hostmass = float(dataline[10])
+            dhostmass = float(dataline[11])
 
-def abs_mag(hostmass):
-    # returns true or false given hostmass
-    # arXiv:1401.4064v2 proposed step function for handling differences
-    # for various host galaxies
-    # useless function but this is just to remind me
-    # to put this in the calculation for mu
-    if (hostmass < 10.):
-        return true
-    else:
-        return false
+    #        dL = dLumi(zcmb,zhel)
 
-def dLumi(zhel, zcmb, lm, func):
-    # takes heliocentric redshift + CMB frame redshift values
-    # and then outputs a luminosity distance!
-    # arxiv 1601.01451 for reference, eq 2,3
-    distance = (1+zhel)*c*sc.integrate.quad(func, 0, zcmb, args=(lm,))/H0
-    return distance
+    # calculating dL requires a guess for omega_m, so I can't do it here, should be called in mcmc,
+    # but you can try using dLumi, inverseHubble
 
-def import_data(inputfile):
-    datafile = open(inputfile, 'r')
-    numLines = 0
-    
-    # returned lists
-    mb_list = []
-    zcmb_list = []
-    zhel_list = []
-    c = []
-    x1_list = []
-
-    for line in datafile:
-        if numLines == 0:
-            continue
-        dataline = list(csv.reader(f, delimiter=" "))
-        #name zcmb zhel dz mb dmb x1 dx1 color dcolor 3rdvar d3rdvar tmax dtmax cov_m_s cov_m_c cov_s_c set ra dec biascor
-        name = dataline[0]
-        zcmb = float(dataline[1])
-        zhel = float(dataline[2])
-        dz = float(dataline[3])
-        mb = float(dataline[4])
-        dmb = float(dataline[5])
-        x1 = float(dataline[6])
-        dx1 = float(dataline[7])
-        color = float(dataline[8])
-        dcolor = float(dataline[9])
-        hostmass = float(dataline[10])
-        dhostmass = float(dataline[11])
-
-#        dL = dLumi(zcmb,zhel)
-
-# calculating dL requires a guess for omega_m, so I can't do it here, should be called in mcmc,
-# but you can try using dLumi, inverseHubble
-
-        # append lists
-        mb_list.append(mb)
-        zcmb_list.append(zcmb)
-        zhel_list.append(zhel)
-        x1_list.append(x1)
-        c.append(color)
-    # returns list
-    return mb_list, zcmb, zhel, c, x1_list
+            # append lists
+            mb_list.append(mb)
+            zcmb_list.append(zcmb)
+            zhel_list.append(zhel)
+            x1_list.append(x1)
+            c.append(color)
+        # returns list
+        return mb_list, zcmb_list, zhel_list, c, x1_list
 
--- python/mcmc.py
+++ python/mcmc.py
@@ -1,36 +1,154 @@
-import sys, data_wrapper
-#dsfsfsfsf
-final_params = []
+import sys, csv, random, math
+from data_wrapper import data_stuff
+from scipy import integrate as integral
+import numpy as np 
+
+# final info from the mcmc
+
+final_params_omega_l = []
+final_params_omega_m = []
+final_params_omega_k = []
 final_l = []
 final_weights = []
+
+#global constants
+
+c = 1
+h = 0.673
+H0 = 100 * h #km/s/Mpc
+
+# for now treating alpha and beta as constants that do not have to be fit for
+# these are estimated values from Nielson et al.
+alpha = 0.1
+beta = 3
+M = -19
+
+data_length = 0
+
+single_run_length=10
 NUM_ITER = 10
-def likelihood:
+
+def likelihood_calc(mu_model,mu_data):
     # evaluates likelihood given the model
+    # basic error
+    likelihood=[]
+    P = 0    
+    n = range(data_length-1)
+    for i in n:
+        p = (mu_data[i]-mu_model[i])*(mu_data[i]-mu_model[i])/mu_model[i]
+        P = P + p;
+    return P
 
-def mu_model:
+
+def omega_draw():
+    om = random.random()
+    ol = 1 - om
+    ok = 1
+    #ok=floor(rand*3)-1
+    return om, ol, ok
+
+def dLumi(zhel, zcmb, om, ol, ok, func):
+    # takes heliocentric redshift + CMB frame redshift values
+    # and then outputs a luminosity distance!
+    # arxiv 1601.01451 for reference, eq 2,3
+    distance, distance_err = integral.quad(func, 0, zcmb, args=(om,ol,ok,))
+    distance = (1+zhel)*c*distance/H0 
+    return distance
+
+def inverseHubble(z,om,ol,ok):
+    # Calculates reduced hubble assuming lambda CDM
+    # om is omega_matter
+    return np.sqrt(om*np.power((1+z),3)+ok*np.power((1+z),2)+ol)
+
+def mu_model_calc(zhel_data,zcmb_data,omega_m,omega_l,omega_k):
     # calculates mu given z
+    mu_model=[];
+    
+    def ii(x):
+        return H0/H(x,omega_m,omega_l,omega_k)
+    
+    n = range(data_length-1)
+    for i in n:
+        #z=M[i]
+        #I, err=sc.integrate.quad(ii(x),0,z)
+        #dL=(1+z)*(dH/sqrt(omega_k))*math.asin(sqrt(omgea_k)*I)
+        dL = dLumi(zhel_data[i],zcmb_data[i],omega_m,omega_l,omega_k,inverseHubble)
+        mu_temp = 25 + 5*np.log10(dL)
+        mu_model.append(mu_temp)
+    return mu_model
 
-def mu_data:
+def mu_data_calc(m_B,x1,C):
     # gives a functional form of mu given data + nuisance parameters
+    mu_data=[]
+    
+    n = range(data_length-1)
+    for i in n:
+        f=m_B[i]-M+alpha*x1[i]-beta*C[i]
+        mu_data.append(f)
+    return mu_data
 
-def mu_error:
+
+def mu_error():
+    return 0
     # gives error associated
     
-def step_mcmc:
+
+def likelihood_draw(zhel_data,zcmb_data,m_B_data,x1_data,c_data):
+    om,ol,ok = omega_draw()
+    #print "calculating model mu"
+    mu = mu_model_calc(zhel_data,zcmb_data,om,ok,ol)
+    #print "calculating estimated mu"
+    mu_hat = mu_data_calc(m_B_data,x1_data,c_data)
+    P = likelihood_calc(mu_hat, mu)
+    return P, mu, mu_hat, om, ol, ok
+
+
+def step_mcmc(single_run_length,zhel_data,zcmb_data,m_B_data,x1_data,c_data):
     # evaluates one step of mcmc algorithm
     # put weight here for now
+    #initialize 
+    P_old, mu, mu_hat, om, ol, ok = likelihood_draw(zhel_data,zcmb_data,m_B_data,x1_data,c_data);
+#    print P_old, mu, mu_hat, om, ol, ok
+    w=0
+    n = range(single_run_length)
+    for i in n:
+        P_new, mu, mu_hat, om, ol, ok = likelihood_draw(zhel_data,zcmb_data,m_B_data,x1_data,c_data)
+        r=P_new/P_old;
+        if r>1:
+            w=w+1;
+        else:
+            w=0;
+            P_old=P_new;
+    return om, ol, ok, w, P_new
 
-    # return p, l, w
     
-def covariance:
+def covariance():
     # think about this later
+    return 0
 
-def main:
+def main():
     # call functions
+    data_wr = data_stuff()
+    m_B_data, zcmb_data, zhel_data, c_data, x1_data = data_wr.import_data("../data/jla_lcparams.txt")
+    global data_length
+    data_length = len(x1_data)
+    print "Sample Size: ", data_length
     n = range(NUM_ITER)
     for i in n:
-        param, likelihood, weight = step_mcmc()
-        final_params.append(param)
+        print "Iteration: ", i+1
+        param_omega_m, param_omega_l, param_omega_k, weight, likelihood = step_mcmc(single_run_length,zhel_data,zcmb_data,m_B_data,x1_data,c_data)
+        final_params_omega_m.append(param_omega_m)
+        final_params_omega_l.append(param_omega_l)
+        final_params_omega_k.append(param_omega_k)
         final_l.append(likelihood)
         final_weights.append(weight)
-    print final_params, final_l, final_weights
+    #print final_params_omega_m,final_params_omega_l,final_params_omega_k, final_l, final_weights
+    weighted_omega_m_avg = 0
+    total_weight = 0
+    for i in range(len(final_params_omega_m)):
+        weighted_omega_m_avg = weighted_omega_m_avg + final_params_omega_m[i]*final_weights[i]
+        total_weight = total_weight + final_weights[i]
+    print "WEIGHTED OMEGA_M AVG: ", weighted_omega_m_avg/total_weight
+
+if __name__ == "__main__":
+    main()
