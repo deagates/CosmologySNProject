36101440ed16d2d3df74f30509493028f993b898
sezata <sezata@gmail.com>
2016-12-06 17:21:48 -0500

    updated paper again

--- plotting/plot_parameter.py
+++ plotting/plot_parameter.py
@@ -16,7 +16,7 @@ def import_data(inputfile):
     # alpha, beta, delta_M, M_prime, omega_m, omega_l, likelihood
     datafile = open(inputfile, 'r')
     numLines = 0
-    
+    iterLines = 0
         # returned lists
     alpha_list = []
     beta_list = []
@@ -25,20 +25,28 @@ def import_data(inputfile):
     om_list = []
     ol_list = []
     p_list = []
+    w_list = []
+
+    final_om = []
+    final_weight = []
 
     for line in datafile:
         numLines = numLines + 1
-        if numLines < 5001:
+        if numLines < 4:
+            continue
+        iterLines = iterLines + 1
+        if line == "new iteration\n":
+            iterLines = 0
             continue
         dataline = line.split()
         alpha = float(dataline[0])
         beta = float(dataline[1])
-        dM = float(dataline[2])
-        Mp = float(dataline[3])
+        Mp = float(dataline[2])
+        dM = float(dataline[3])
         om = float(dataline[4])
         ol = float(dataline[5])
         p = float(dataline[6])
-
+        w = float(dataline[7])
         alpha_list.append(alpha)
         beta_list.append(beta)
         dM_list.append(dM)
@@ -46,56 +54,62 @@ def import_data(inputfile):
         om_list.append(om)
         ol_list.append(ol)
         p_list.append(p)
+        w_list.append(w)
+        if iterLines == 20000:
+            final_om.append(om)
+            final_weight.append(w)
+            iterLines = 0
+#            break
 
-        if numLines == 10000:
-            break
-
-        # returns list
+    print final_weight, final_om
+    
     return alpha_list,beta_list,dM_list,Mp_list,om_list,ol_list,p_list
 
 def main(inputfile):
     a,b,dM,Mp,om,ol,p = import_data(inputfile)
-    n = len(a)
-    plt.plot(range(n),a)
-    plt.xlabel("Steps")
-    plt.ylabel("Alpha")
-    plt.axis([0,n,0.,4.])
-    plt.savefig("alpha.pdf")    
-    plt.clf()
+    # n = len(a)
+    # plt.rcParams.update({'font.size': 18})
+    # plt.plot(range(n),a)
+    # plt.xlabel("Steps")
+    # plt.ylabel(r"$\alpha$")
+    # plt.axis([0,n,-1.,4.])
+    # plt.savefig("plots/alpha.pdf")    
+    # plt.clf()
 
-    plt.plot(range(n),b)
-    plt.xlabel("Steps")
-    plt.ylabel("Beta")
-    plt.axis([0,n,1.,5.])
-    plt.savefig("beta.pdf")
-    plt.clf()
+    # plt.plot(range(n),b)
+    # plt.xlabel("Steps")
+    # plt.ylabel(r"$\beta$")
+    # plt.axis([0,n,1.,6.])
+    # plt.savefig("plots/beta.pdf")
+    # plt.clf()
 
-    plt.plot(range(n),dM)
-    plt.xlabel("Steps")
-    plt.ylabel(r"$\Delta$M")
-    plt.axis([0,n,-1.,0.])
-    plt.savefig("dM.pdf")    
-    plt.clf()
+    # plt.plot(range(n),dM)
+    # plt.xlabel("Steps")
+    # plt.ylabel(r"$\Delta$M")
+    # plt.axis([0,n,-2.,1.])
+    # plt.savefig("plots/dM.pdf")    
+    # plt.clf()
 
-    plt.plot(range(n),Mp)
-    plt.xlabel("Steps")
-    plt.ylabel("M'")
-    plt.axis([0,n,-20.,-18.])
-    plt.savefig("Mp.pdf")
-    plt.clf()
+    # plt.plot(range(n),Mp)
+    # plt.xlabel("Steps")
+    # plt.ylabel("M'")
+    # plt.axis([0,n,-21.,-17.])
+    # plt.savefig("plots/Mp.pdf")
+    # plt.clf()
 
-    plt.plot(range(n),om)
-    plt.xlabel("Steps")
-    plt.ylabel(r"$\Omega_m$")
-    plt.axis([0,n,0.,1.])
-    plt.savefig("om.pdf")
-    plt.clf()
+    # plt.plot(range(n),om)
+    # plt.xlabel("Steps")
+    # plt.ylabel(r"$\Omega_m$")
+    # plt.axis([0,n,0.,1.])
+    # plt.savefig("plots/om.pdf")
+    # plt.clf()
 
-    plt.plot(range(n),ol)
-    plt.xlabel("Steps")
-    plt.ylabel(r"$\Omega_l$")
-    plt.axis([0,n,0.,1.])
-    plt.savefig("ol.pdf")
+    # plt.plot(range(n),ol)
+    # plt.xlabel("Steps")
+    # plt.ylabel(r"$\Omega_l$")
+    # plt.axis([0,n,0.,1.])
+    # plt.savefig("plots/ol.pdf")
 if __name__ == "__main__":
-    main("results3.txt")
+    main("results/Dec6_MCMC_Run_06.txt")
+#    main("results/Dec6_MCMC_Run_08.txt")
 
